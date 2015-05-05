3a3f80d3c8ed5ef8f44fb489b90013796c0edea8
Jekkoo <celikhanekinci@hotmail.com>
2015-05-05 23:25:19 +0300

    Create prob.py

---
+++ prob.py
@@ -0,0 +1,35 @@
+
+#Outputs frequencies
+import collections
+import matplotlib.pyplot as plt
+testlist = [1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 3, 4, 4, 4, 4, 5, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 8, 8, 9, 9]
+c = collections.Counter(testlist)
+print c
+# calculate the number of instances in the list
+count_sum = sum(c.values())
+for k,v in c.iteritems():
+  print "The frequency of number " + str(k) + " is " + str(float(v) / count_sum)
+
+
+#Boxlplot
+plt.boxplot(testlist)
+plt.show()
+
+
+#Histogram
+plt.hist(testlist, histtype='bar')
+plt.show()
+
+
+#QQ-plot
+import numpy as np 
+import scipy.stats as stats
+
+plt.figure()
+test_data = np.random.normal(size=1000)   
+graph1 = stats.probplot(test_data, dist="norm", plot=plt)
+plt.show() #this will generate the first graph
+plt.figure()
+test_data2 = np.random.uniform(size=1000)   
+graph2 = stats.probplot(test_data2, dist="norm", plot=plt)
+plt.show() #this will generate the second graph
